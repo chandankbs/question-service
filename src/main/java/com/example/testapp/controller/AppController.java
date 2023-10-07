e54aec25cd37afd6cc94a12f866de6578fcbfe93
chandankbs <chandan1998bs@gmail.com>
2023-10-07 23:55:37 +0530

    initial commit

---
+++ src/main/java/com/example/testapp/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.example.testapp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+	
+	@GetMapping ("/")
+	public List<String> getHello() {
+		List<String> list = new ArrayList<String>();
+		list.add("modi");
+		list.add("yogi ji");
+		
+		return list;
+	}
+
+}
